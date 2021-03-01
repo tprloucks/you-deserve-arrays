d4b9459090eeeb61b1d502db55311af423aef94f
Michael Loucks <michael.loucks@codeimmersives.com>
2021-03-01 13:53:44 -0600

    finished

--- main.js
+++ main.js
@@ -1,11 +1,46 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+function getFirstItemFrom(arr){
+  return arr[0]
+}
 
+function getLastItemFrom(arr){
+  return arr[arr.length -1]
+}
 
+function getIndex3(arr){
+  if (arr.length >=4){
+    return arr[3]
+  }
+  else{
+    return arr[arr.length -1]
+  }
+}
 
+function isLongList(arr){
+  if (arr.length >= 10){
+    return true
+  }else{
+    return false
+  }
+}
 
+function firstItemIsNumber(arr){
+  if(arr[0] === 'number'){
+    return true
+  }else{
+    return false
+  }
+}
 
+function secondChar0fThirdString(arr){
+  if(typeof mixedarr[1] === "number"){
+    return true
+  }else{
+    return false;
+  }
+}
 
 
  /********************************************
